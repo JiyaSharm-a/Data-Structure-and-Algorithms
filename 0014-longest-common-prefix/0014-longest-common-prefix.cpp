a4a7071b02dd2218d931273fe0b6e1bf020ec385
JiyaSharm-a <120440034+JiyaSharm-a@users.noreply.github.com>
2024-07-31 22:52:49 +0530

    Time: 7 ms (28.25%), Space: 11.3 MB (35.82%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-
-        // someone else solution
-        // Since is sorted, if you have 'fl' in the first string, having 'fl' in the 
-        // last string means that all the in-between strings also have 'fl'.
-        // So you pretty much can only compare these two and ignore the rest.
-        
         sort(strs.begin(),strs.end());
-        int a=strs.size();
-        string n=strs[0],m=strs[a-1],ans="";
 
-        for(int i=0;i<n.size();i++)
-        {
-            if(n[i]==m[i])
+        string ans = "";
+    
+            for(int j=0; j<strs[0].size(); j++)
             {
-                ans+=n[i];
+                if(strs[0][j] == strs[strs.size()-1][j])
+                {
+                    ans += strs[0][j];
+                }
+                else
+                break;
             }
-            else break;
-        }
-        return ans;
+
+            return ans;
         
     }
 };
-
