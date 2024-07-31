4a6f61fb817921cfbb502789598d44b311f04914
JiyaSharm-a <120440034+JiyaSharm-a@users.noreply.github.com>
2024-07-31 12:45:58 +0530

    Time: 825 ms (5.02%), Space: 54.2 MB (5.92%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -17,27 +17,26 @@ public:
 
         return ans;
     }
-    bool memo(vector<int> &nums, int index, vector<int> &dp) {
-        // base case: reached the end of the array
-        if (index >= nums.size() - 1) {
-            return true;
-        }
+    bool memo(vector<int> &nums,int index,vector<int> &dp)
+    {
+        // base case
+        if(index >= nums.size() - 1)
+        return true;
 
-        // if current position has zero steps to move, we can't move forward
-        if (nums[index] == 0) {
-            return false;
-        }
+        if(nums[index] == 0)
+        return false;
 
-        // check if the result is already computed
-        if (dp[index] != -1) {
-            return dp[index];
-        }
+        if(dp[index] != -1)
+        return dp[index];
 
-        // recursive case
         bool ans = false;
-        for (int i = 1; i <= nums[index]; ++i) {
-            ans = ans || memo(nums, index + i, dp);
-            if (ans) {
+        for(int i=1; i<=nums[index]; i++)
+        {
+            ans = ans || memo(nums,index+i,dp);
+
+            // if ans found i.e if true then break
+            if (ans) 
+            {
                 break;
             }
         }
@@ -45,9 +44,8 @@ public:
         dp[index] = ans;
         return dp[index];
     }
-
     bool canJump(vector<int>& nums) {
-        vector<int> dp(nums.size(), -1);  // Initialize dp array with -1
-        return memo(nums, 0, dp);
+        vector<int> dp(nums.size(), -1);
+        return memo(nums,0,dp);
     }
 };
